81cad742508530e9ff9d9a5f71ee0db1f0ba6380
Dan Buch <dan@meatballhat.com>
2014-05-24 16:23:56 -0400

    Yet more logging to figure out why integrations fail on Travis

--- store/file_store.go
+++ store/file_store.go
@@ -8,18 +8,22 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/dustin/go-humanize"
 	"github.com/meatballhat/artifacts-service/artifact"
 )
 
 // FileStore stores stuff as files.  Wow!
 type FileStore struct {
 	Prefix string
+	log    *logrus.Logger
 }
 
 // NewFileStore returns a *FileStore.  AMAZE.
-func NewFileStore(prefix string) *FileStore {
+func NewFileStore(prefix string, log *logrus.Logger) *FileStore {
 	return &FileStore{
 		Prefix: prefix,
+		log:    log,
 	}
 }
 
@@ -30,11 +34,18 @@ func (fs *FileStore) Store(a *artifact.Artifact) error {
 
 	err := os.MkdirAll(fullPathPrefix, 0755)
 	if err != nil {
+		fs.log.WithFields(logrus.Fields{
+			"err":    err,
+			"prefix": fullPathPrefix,
+		}).Error("failed to make dest file path prefix")
 		return err
 	}
 
 	fd, err := ioutil.TempFile("", "artifacts-tmp")
 	if err != nil {
+		fs.log.WithFields(logrus.Fields{
+			"err": err,
+		}).Error("failed to get tempfile")
 		return err
 	}
 
@@ -42,9 +53,29 @@ func (fs *FileStore) Store(a *artifact.Artifact) error {
 
 	_, err = io.CopyN(fd, a.Instream, int64(a.Size))
 	if err != nil {
+		fs.log.WithFields(logrus.Fields{
+			"err":       err,
+			"temp_file": fd.Name(),
+		}).Error("failed to copy to temporary file")
 		return err
 	}
 
 	defer os.Remove(fd.Name())
-	return os.Rename(fd.Name(), fullPath)
+	err = os.Rename(fd.Name(), fullPath)
+	if err != nil {
+		fs.log.WithFields(logrus.Fields{
+			"err":       err,
+			"temp_file": fd.Name(),
+			"dest":      fullPath,
+		}).Error("failed to move temporary file to dest")
+	}
+
+	fs.log.WithFields(logrus.Fields{
+		"source": a.Source,
+		"prefix": fs.Prefix,
+		"dest":   a.Fullpath(),
+		"size":   humanize.Bytes(a.Size),
+	}).Info("stored artifact to file")
+
+	return nil
 }
